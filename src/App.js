78a7a26b9ae213e177dc23c9d2a0395189f3daa7
AnitaMartinez <amartinezaguilar6@gmail.com>
2020-03-13 12:48:01 +0100

    Create basic app with table and form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
 
-function App() {
+const usersData = [
+  { id: 1, name: 'Tania', username: 'floppydiskette' },
+  { id: 2, name: 'Craig', username: 'siliconeidolon' },
+  { id: 3, name: 'Ben', username: 'benisphere' },
+]
+
+const App = () => {
+
+  const [users, setUsers ] = useState(usersData)
+
+  const addUser = user => {
+    user.id = user.length + 1
+    setUsers([...users, user])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+          <AddUserForm onAddUser={addUser} />
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users}/>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/forms/AddUserForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const AddUserForm = props => {
+
+    const initialFormState = {name: '', username: ''}
+
+    const [user, setUser] = useState(initialFormState)
+
+    const handleInputChange = event => {
+        const {name, value } = event.target
+        setUser({...user, [name]: value})
+    }
+
+    const handleSubmit = event => {
+        const {onAddUser} =  props
+
+        event.preventDefault()
+        if(!user.name || !user.username) return
+        onAddUser(user)
+        setUser(initialFormState)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label id="name">Name</label>
+            <input
+                onChange={handleInputChange}
+                type="text"
+                name="name" value={user.name}
+            />
+            <label id="username">Username</label>
+            <input
+                onChange={handleInputChange}
+                type="text"
+                name="username"
+                value={user.username}
+            />
+            <button>Add new user</button>
+        </form>
+    )
+}
+
+export default AddUserForm
