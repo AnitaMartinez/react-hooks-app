ff3a5dab0198ea5cc2ae44b3b1cf6694a6229269
AnitaMartinez <amartinezaguilar6@gmail.com>
2020-03-27 13:37:51 +0100

    Updates

---
+++ precache-manifest.0228b3f1400a9ba89a1ff61618423e63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e444a292e3aa1192d938597bfd1972",
+    "url": "/react-hooks-app/index.html"
+  },
+  {
+    "revision": "24fd9d12a8a263acd779",
+    "url": "/react-hooks-app/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "87f09d8466300177b93b",
+    "url": "/react-hooks-app/static/js/2.649f72a0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-hooks-app/static/js/2.649f72a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24fd9d12a8a263acd779",
+    "url": "/react-hooks-app/static/js/main.4b50d1d2.chunk.js"
+  },
+  {
+    "revision": "cf44bf64277f2dc0a00c",
+    "url": "/react-hooks-app/static/js/runtime-main.8bb41b98.js"
+  }
+]);
